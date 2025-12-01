e2f978c5a120a4b8dfd2162098b12538b71db55f
Romain Bosa <romain.bosa@gmail.com>
2025-12-01 19:10:30 +0100

    Day01

---
+++ Day01/Program.cs
@@ -0,0 +1,18 @@
+﻿int dial = 50;
+int password1 = 0;
+int password2 = 0;
+char direction = 'R';
+
+foreach (string rotation in File.ReadAllLines("day01.txt"))
+{
+    if (rotation[0] != direction)
+    {
+        dial = (dial == 0) ? 0 : 100 - dial;
+        direction = rotation[0];
+    }
+    password2 += Math.DivRem(dial + int.Parse(rotation[1..]), 100, out dial);
+    password1 += (dial == 0) ? 1 : 0;
+}
+
+Console.WriteLine($"Part 1: {password1}");
+Console.WriteLine($"Part 2: {password2}");
---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2025 in C#
+
+My C# solutions to [Advent of Code](https://adventofcode.com/) 2025.
